ef7ffc6784c45691cd2b8a78ddc0c463de659a9a
Christoph Geske <christophgeske2@googlemail.com>
2024-04-05 00:59:31 +0200

    Simplefied code and added more comments

--- helloLMStudio.py
+++ helloLMStudio.py
@@ -6,15 +6,27 @@ from openai import OpenAI
 # the api_key
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
+
+# Display the initial message for the user in the terminal.
+print("I am a small LLM ask me anything.")
+
 # The history is a list of messages that have been exchanged between the user and the assistant.
-history = [{"role": "system",
+history = []
+'''
+    [{"role": "system",
      "content": "You are an intelligent assistant. You always provide well-reasoned answers that are both correct and helpful. You are probably not GPT-4 but rather a smaller open source model."},
     {"role": "user",
      "content": "Hallo, How are you?"},]
-
+'''
 # While Loop: Keeps the chat session active, allowing for continuous interaction.
 while True:
+    # Get the user's first message and add it to the history.
+    user_input = input("> ")
+    history.append({"role": "user", "content": user_input})
+
+    # Request a chat completion from the local server using the chat history.
     completion = client.chat.completions.create(
+        # Here your need to ad your model ID You find it in the LMStudio.
         model="bartowski/Starling-LM-7B-beta-GGUF/Starling-LM-7B-beta-IQ4_XS.gguf",
         # The message is always the complete chat history. That is the simplest way short term memory can be implemented.
         messages=history,
@@ -56,4 +68,4 @@ while True:
     # Catches user’s next message and appends it to the chat history.
     # When the input() function is called, Python will pause and wait for the user to enter some text and press Enter
     # before continuing; therefore, the while loop is not constantly requiring resources.
-    history.append({"role": "user", "content": input("> ")})
+# history.append({"role": "user", "content": input("> ")})
